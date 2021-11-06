4228bb67c3d425b3aa6c58f9c7311967536558b0
kontino <kostis.gouz@gmail.com>
2021-11-06 22:20:08 +0200

    hamming

--- HashFunctions/HashHC.cpp
+++ HashFunctions/HashHC.cpp
@@ -30,3 +30,45 @@ unsigned int HashHC::Hash(const std::vector<int> &p){
 
 	return tag;
 }
+
+// Given bucket tag (index), returns a vector of all the closest vertices (all their bucket indices)
+std::vector<int> HashHC::HammingNeighbors(int index, int probes, int dim){
+
+    // error handling: if probes >= 2^d -> null
+
+    std::vector <int> neighborProbes;
+    int distance = 1;
+    
+    while(neighborProbes.size() < probes){
+        MaskPermutations(distance, &neighborProbes, probes);
+        distance++;
+    }
+    for (int i=0; i<neighborProbes.size(); i++){
+        neighborProbes[i] ^= index;
+    }
+
+    return neighborProbes;
+}
+
+// Truncates to vector <masks> integers that can generate integers for with [remBits] Hamming distance
+void HashHC::MaskPermutations(int remBits, std::vector<int> *masks, int &threshold, int pos=0, int val=0){
+
+    if (masks->size() == threshold)
+        return;
+
+    if(pos == k || remBits == 0){
+        masks->push_back(val);
+        threshold ++;
+        return;
+    }
+
+    int set;
+
+    set = 1 << pos;
+    val &= set;
+    MaskPermutations(remBits--, masks, threshold, pos+1, val);
+
+    set = ~set;
+    val &= set;
+    MaskPermutations(remBits, masks, threshold, pos+1, val);
+}
--- HashFunctions/HashHC.h
+++ HashFunctions/HashHC.h
@@ -7,11 +7,11 @@
 class HashHC: public HashInterface{
     private:
         std::unordered_map<int, unsigned int> *h_mapping;
+        void MaskPermutations(int remBits, std::vector<int> *masks, int &threshold, int pos=0, int val=0);
     public:
         HashHC(int w, int k, int TableSize, int vecSize);
         ~HashHC();
         unsigned int Hash(const std::vector<int> &p);
         inline unsigned int HashIndex(unsigned int ID){return ID;};
+        std::vector<int>  HammingNeighbors(int index, int probes, int dim);
 };
-
-
